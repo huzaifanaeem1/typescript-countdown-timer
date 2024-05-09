b3c8f8d4d7497277120ccda62aed17c1cb448d55
Huzaifa Naeem <159990985+huzaifanaeem1@users.noreply.github.com>
2024-05-10 01:51:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+//countdown timer project
+//step # 1
+import { differenceInSeconds } from "date-fns";
+import chalk from "chalk";
+//function for  countdown second
+function* countdownTimer(second) {
+    //while loop
+    while (second > 0) {
+        yield second;
+        second--;
+    }
+}
+//step # 2
+//counter timing initialization
+let timerIterator = countdownTimer(10);
+//function to create a countdown timer
+function displatCountdown() {
+    // value below 10
+    let result = timerIterator.next();
+    // if / else statement
+    if (!result.done) {
+        //current date and time call
+        const now = new Date();
+        //claculate minutes in time
+        const countdownTimer = new Date(now.getTime() + (result.value * 1000));
+        // calculate remaining seconds in time
+        const remainingSeconds = differenceInSeconds(countdownTimer, now);
+        console.log((chalk.yellow)(`Remaining Seconds: ${remainingSeconds}`));
+        setTimeout(displatCountdown, 1000); // 1sec is = to 1000 ms (mili seconds) k = hoga..
+    }
+    else {
+        //display result countdown complete
+        console.log((chalk.red)("Countdown Compelete!"));
+    }
+}
+//Invoke
+displatCountdown();
